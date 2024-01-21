a311602c7c2dd947288efe51543449ae4ac7f6ff
warsaw <princemishra90761@gmail.com>
2024-01-21 23:02:13 +0530

    learnt hooks and made an easy add or subtract value project

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,10 +1,33 @@
-
+import { useState } from "react"
 
 function App() {
-  
+  let [counter, setcounter] = useState(15)
+
+
+  function addValue() {
+    setcounter((p) => {
+      if (p == 20) {
+        return (p = p)
+      }
+      else {
+        return (p = p + 1)
+      }
+    })
+  }
+
+  const removeValue = () => {
+    setcounter(counter - 1)
+  }
 
   return (
-    <h1>chai or react with vite</h1>
+    <>
+      <h1>chai aur react with vite</h1>
+      <h2> counter value:{counter}</h2>
+
+      <button onClick={addValue}>add value</button>
+      <br />
+      <button onClick={removeValue}>remove value</button>
+    </>
   )
 }
 
