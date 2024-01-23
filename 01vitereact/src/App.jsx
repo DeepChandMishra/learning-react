f3e74c383e84bc44aab871688e6edf64ea02cdb0
warsaw <princemishra90761@gmail.com>
2024-01-23 23:55:04 +0530

    made password generator project and useCallback,useEffect hooks learnt

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,13 +1,34 @@
-import { useState } from "react"
+import { useCallback, useEffect, useState } from "react"
+
 
 function App() {
-  const [color,setcolor]=useState("olive")
+  const [length,setLength]=useState(8)
+  const [num,setNum]=useState(false)
+  const [character,setCharacter] = useState(false)
+  const [password,setPassword]=useState("")
 
+const passwordGenerator = useCallback(()=>{
+    let pass=""
+    let str="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    if(num) str += "0123456789"
+    if(character) str += "!@#$%^&*(){}[]~`=+"
+
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length +1)
+      pass += str.charAt(char)
+
+    }
+
+    setPassword(pass)
+
+},[length,num,character,setPassword])
+
+   useEffect(() => {passwordGenerator()},[length,num,character,setPassword])
   return (
     <>
-      {/*<h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>tailwind test</h1>
+    {/* <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>tailwind test</h1>
       <Card username="himani" btnText="click me" />
-      <Card username="munni" btnText="visit me" /> */}
+      <Card username="munni" btnText="visit me" /> 
 
     <div className="w-full h-screen duration-200" style={{backgroundColor:color}}>
       <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
@@ -21,8 +42,59 @@ function App() {
           <button onClick={()=>{setcolor("white")}} className="outline-none px-4 py-1 rounded-full  shadow-lg" style={{backgroundColor: "white"}}>White</button>
         </div>
       </div>
+    </div> */}
+
+     
+
+    <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 text-orange-500 bg-gray-500 ">
+      <h1 className="text-white text-center">Password generator</h1>
+      <div className="flex shadow rounded-lg overflow-hidden mb-4">
+          <input 
+          type="text"
+          value={password} 
+          className="outline-none w-full py-1 px-3"
+          placeholder="password"
+          readOnly
+          />  
+
+          <button className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0">copy</button> 
+
+      </div>
+  
+    <div className="flex test-sm gap-x-2">
+      <div className="flex items-center gap-x-1">
+        <input 
+        type="range"
+        min={6}
+        max={100}
+        value={length}
+        className="cursor-pointer"
+        onChange={(e) =>{setLength(e.target.value)}}
+        />
+        <label>Length:{length}</label>
+      </div>
+      <div className="flex items-center gap-x-1">
+        <input 
+        type="checkbox" 
+        defaultChecked={num}
+        id="numberInput" 
+        onChange={()=>{setNum((prev)=> !prev)}}
+        />
+        <label htmlFor="numberInput">Numbers</label>
+      </div>
+      <div className="flex items-center gap-x-1">
+        <input 
+        type="checkbox" 
+        defaultChecked={character}
+        id="characterInput" 
+        onChange={()=>{setCharacter((prev)=> !prev)}}
+        />
+        <label htmlFor="characterInput">Characters</label>
+      </div>
     </div>
 
+    </div>
+      
     </>
   )
 }
